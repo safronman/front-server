cc05c2004864037b4cae0ea8bfbc5584f3b72058
safronman <safronmanbox@gmail.com>
2020-05-28 15:19:05 +0300

    update user

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,13 @@ function App() {
 
 
     const getUsers = () => {
-        const search= window.location.search
+        const search = window.location.search
 
         axios.get('http://127.0.0.1:7542/users' + search)
             .then((res) => {
                 setUsers(res.data)
             })
     }
-
 
     const createUser = () => {
         axios.post('http://127.0.0.1:7542/users', {name: userName})
@@ -41,24 +40,30 @@ function App() {
         setUserName(e.currentTarget.value)
     }
 
+    const updateUser = (name: string, id: string) => {
+        axios.put(`http://127.0.0.1:7542/users`, {name, id})
+            .then((res) => {
+                getUsers()
+            })
+    }
+
+    const usersJsx = users.map((user: any) => {
+        return <div key={user.id} className="userWrapper">
+            <p>user id: <b>{user._id}</b></p>
+            user name: <input type={"text"} defaultValue={user.name}
+                              onBlur={(e) => updateUser(e.currentTarget.value, user._id)}/>
+            <button onClick={() => {
+                onDeleteUser(user._id)
+            }}>Delete user
+            </button>
+        </div>
+    })
+
     return (
         <div>
             <input type="text" placeholder={'enter user name'} value={userName} onChange={addUserName}/>
             <button onClick={createUser}>Create user</button>
-            <div>
-                {
-                    users.map((user: any) => {
-                        return <div key={user.id} className="userWrapper">
-                            <p>user id: <b>{user._id}</b></p>
-                            <p>user name: <b>{user.name}</b></p>
-                            <button onClick={() => {
-                                onDeleteUser(user._id)
-                            }}>Delete user
-                            </button>
-                        </div>
-                    })
-                }
-            </div>
+            <div>{usersJsx}</div>
         </div>
     );
 }
